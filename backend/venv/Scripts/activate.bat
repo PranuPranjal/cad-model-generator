942c5afd37e045a4a12ebf4e91d6e180d789b60d
Ganji sri vijaya sai chandra <saichandra38ganji@gmail.com>
2025-10-13 21:11:07 +0530

    Initial commit: Add project files

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\naguk\Desktop\Major project\cad-model-generator\backend\venv
+set VIRTUAL_ENV=E:\CAD GPT\cad-model-generator\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(venv) 
 
 :END
 if defined _OLD_CODEPAGE (
